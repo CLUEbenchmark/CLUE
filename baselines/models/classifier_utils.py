173af3ae46ebbea4875aa27b7ae23fdced2b7a79
DukeEnglish <ljyduke@gmail.com>
2019-11-30 10:55:21 +0800

    add copa runcallisier sh for bert and data processor

--- baselines/models/classifier_utils.py
+++ baselines/models/classifier_utils.py
@@ -828,3 +828,52 @@ class WSCProcessor(DataProcessor):
       examples.append(
           InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
     return examples
+
+class COPAProcessor(DataProcessor):
+  """Processor for the internal data set. sentence pair classification"""
+
+  def __init__(self):
+    self.language = "zh"
+
+  def get_train_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_json(os.path.join(data_dir, "train.txt")), "train")
+    # dev_0827.tsv
+
+  def get_dev_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_json(os.path.join(data_dir, "dev.txt")), "dev")
+
+  def get_test_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_json(os.path.join(data_dir, "test.txt")), "test")
+
+  def get_labels(self):
+    """See base class."""
+    return ["0", "1"]
+    
+  @classmethod
+  def _create_examples(self, lines, set_type):
+    examples = []
+    for (i, line) in enumerate(lines):
+      i=2 * i
+      guid1 = "%s-%s" % (set_type, i)
+      guid2 = "%s-%s" % (set_type, i+1)
+#         try:
+      text_a = convert_to_unicode(line['premise'])
+      text_b = convert_to_unicode(line['choice0'])
+      label = convert_to_unicode(str(1 if line['label'] == 0 else 0)) if set_type != 'test' else '-1'
+      text_a2 = convert_to_unicode(line['premise'])
+      text_b2 = convert_to_unicode(line['choice1'])
+      label2 = convert_to_unicode(str(0 if line['label'] == 0 else 1)) if set_type != 'test' else '-1'
+      examples.append(
+        InputExample(guid=guid1, text_a=text_a, text_b=text_b, label=label))
+      examples.append(
+        InputExample(guid=guid2, text_a=text_a2, text_b=text_b2, label=label2))
+#         except Exception as e:
+#             print('###error.i:',e, i, line)
+    return examples
+
