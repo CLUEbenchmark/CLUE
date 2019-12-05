5b4911b8500de6ffbce2adb34654f499466c8e07
boshi <bo.shi@mobvoi.com>
2019-12-05 11:20:50 +0800

    update roberta

--- baselines/models/roberta/run_classifier_cmnli.sh
+++ baselines/models/roberta/run_classifier_cmnli.sh
@@ -2,7 +2,7 @@
 # @Author: bo.shi
 # @Date:   2019-11-04 09:56:36
 # @Last Modified by:   bo.shi
-# @Last Modified time: 2019-12-03 17:57:17
+# @Last Modified time: 2019-12-05 11:20:14
 
 TASK_NAME="cmnli"
 MODEL_NAME="roeberta_zh_L-24_H-1024_A-16"
@@ -52,13 +52,11 @@ echo "Finish download model."
 # run task
 cd $CURRENT_DIR
 echo "Start running..."
-if [ $1 == "predict" ]; then
-    echo "Start predict..."
+if [ $# == 0 ]; then
     python run_classifier.py \
       --task_name=$TASK_NAME \
-      --do_train=false \
-      --do_eval=false \
-      --do_predict=true \
+      --do_train=true \
+      --do_eval=true \
       --data_dir=$GLUE_DATA_DIR/$TASK_NAME \
       --vocab_file=$ROBERTA_LARGE_DIR/vocab.txt \
       --bert_config_file=$ROBERTA_LARGE_DIR/bert_config_large.json \
@@ -68,11 +66,13 @@ if [ $1 == "predict" ]; then
       --learning_rate=2e-5 \
       --num_train_epochs=3.0 \
       --output_dir=$CURRENT_DIR/${TASK_NAME}_output/
-else
+elif [ $1 == "predict" ]; then
+    echo "Start predict..."
     python run_classifier.py \
       --task_name=$TASK_NAME \
-      --do_train=true \
-      --do_eval=true \
+      --do_train=false \
+      --do_eval=false \
+      --do_predict=true \
       --data_dir=$GLUE_DATA_DIR/$TASK_NAME \
       --vocab_file=$ROBERTA_LARGE_DIR/vocab.txt \
       --bert_config_file=$ROBERTA_LARGE_DIR/bert_config_large.json \
