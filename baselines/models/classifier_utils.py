b240264e2e1a7681871f046b05270089ff6a5718
Arcon <632112808@qq.com>
2019-12-05 14:57:37 +0800

    Update classifier_utils.py

--- baselines/models/classifier_utils.py
+++ baselines/models/classifier_utils.py
@@ -413,7 +413,7 @@ class CslProcessor(DataProcessor):
   def get_dev_examples(self, data_dir):
     """See base class."""
     return self._create_examples(
-        self._read_json(os.path.join(data_dir, "val.json")), "dev")
+        self._read_json(os.path.join(data_dir, "dev.json")), "dev")
 
   def get_test_examples(self, data_dir):
     """See base class."""
