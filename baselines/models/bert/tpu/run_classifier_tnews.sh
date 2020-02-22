879060050c3111bb37c9f559748f20b34a27954e
DukeEnglish <ljyduke@gmail.com>
2020-02-22 10:37:59 +0800

    modify tpu

--- baselines/models/bert/tpu/run_classifier_tnews.sh
+++ baselines/models/bert/tpu/run_classifier_tnews.sh
@@ -1,9 +1,69 @@
 CURRENT_DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 CURRENT_TIME=$(date "+%Y%m%d-%H%M%S")
 TASK_NAME="tnews"
-export PREV_TRAINED_MODEL_DIR=gs://models_zxw/prev_trained_models/nlp/bert-base/chinese_L-12_H-768_A-12
-export DATA_DIR=gs://data_zxw/nlp/chineseGLUEdatasets.v0.0.1/hard_${TASK_NAME}_1
-export OUTPUT_DIR=gs://models_zxw/fine_tuning_models/nlp/bert-base/chinese_L-12_H-768_A-12/tpu/$TASK_NAME/$CURRENT_TIME
+
+# please create folder 
+export PREV_TRAINED_MODEL_DIR=gs://models_ljy/prev_trained_models/nlp/bert-base/chinese_L-12_H-768_A-12
+export DATA_DIR=gs://data_ljy/nlp/chineseGLUEdatasets.v0.0.1/${TASK_NAME}
+export OUTPUT_DIR=gs://models_ljy/fine_tuning_models/nlp/bert-base/chinese_L-12_H-768_A-12/tpu/$TASK_NAME/$CURRENT_TIME
+
+
+MODEL_NAME="chinese_L-12_H-768_A-12"
+CURRENT_DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+export CUDA_VISIBLE_DEVICES="0"
+export BERT_PRETRAINED_MODELS_DIR=$CURRENT_DIR/prev_trained_model
+export BERT_BASE_DIR=$BERT_PRETRAINED_MODELS_DIR/$MODEL_NAME
+export GLUE_DATA_DIR=$CURRENT_DIR/../../CLUEdataset
+
+# download and unzip dataset
+if [ ! -d $GLUE_DATA_DIR ]; then
+  mkdir -p $GLUE_DATA_DIR
+  echo "makedir $GLUE_DATA_DIR"
+fi
+cd $GLUE_DATA_DIR
+if [ ! -d $TASK_NAME ]; then
+  mkdir $TASK_NAME
+  echo "makedir $GLUE_DATA_DIR/$TASK_NAME"
+fi
+cd $TASK_NAME
+if [ ! -f "train.json" ] || [ ! -f "dev.json" ] || [ ! -f "test.json" ]; then
+  rm *
+  wget https://storage.googleapis.com/cluebenchmark/tasks/tnews_public.zip
+  unzip tnews_public.zip
+  rm tnews_public.zip
+else
+  echo "data exists"
+fi
+echo "Finish download dataset."
+
+# download model
+if [ ! -d $BERT_PRETRAINED_MODELS_DIR ]; then
+  mkdir -p $BERT_PRETRAINED_MODELS_DIR
+  echo "makedir $BERT_PRETRAINED_MODELS_DIR"
+fi
+cd $BERT_PRETRAINED_MODELS_DIR
+if [ ! -d $MODEL_NAME ]; then
+  wget https://storage.googleapis.com/bert_models/2018_11_03/chinese_L-12_H-768_A-12.zip
+  unzip chinese_L-12_H-768_A-12.zip
+  rm chinese_L-12_H-768_A-12.zip
+else
+  cd $MODEL_NAME
+  if [ ! -f "bert_config.json" ] || [ ! -f "vocab.txt" ] || [ ! -f "bert_model.ckpt.index" ] || [ ! -f "bert_model.ckpt.meta" ] || [ ! -f "bert_model.ckpt.data-00000-of-00001" ]; then
+    cd ..
+    rm -rf $MODEL_NAME
+    wget https://storage.googleapis.com/bert_models/2018_11_03/chinese_L-12_H-768_A-12.zip
+    unzip chinese_L-12_H-768_A-12.zip
+    rm chinese_L-12_H-768_A-12.zip
+  else
+    echo "model exists"
+  fi
+fi
+echo "Finish download model."
+
+# upload model and data
+gsutil -m cp $BERT_PRETRAINED_MODELS_DIR/* $PREV_TRAINED_MODEL_DIR
+
+gsutil -m cp $GLUE_DATA_DIR/$TASK_NAME/* $DATA_DIR
 
 python $CURRENT_DIR/../run_classifier.py \
   --task_name=$TASK_NAME \
