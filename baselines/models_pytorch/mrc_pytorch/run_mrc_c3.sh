cbc6871727df1ae1649b8d8ca38cd542a427c830
LowinLi <li783170560@126.com>
2021-09-04 22:20:11 +0800

    fix 修复shell脚本下载mrc数据集

--- baselines/models_pytorch/mrc_pytorch/run_mrc_c3.sh
+++ baselines/models_pytorch/mrc_pytorch/run_mrc_c3.sh
@@ -7,6 +7,36 @@ export BERT_DIR=$OUTPUT_DIR/prev_trained_model/$MODEL_NAME
 export GLUE_DIR=$CURRENT_DIR/mrc_data # set your data dir
 TASK_NAME="c3"
 
+# download and unzip dataset
+if [ ! -d $GLUE_DIR ]; then
+  mkdir -p $GLUE_DIR
+  echo "makedir $GLUE_DIR"
+fi
+cd $GLUE_DIR
+if [ ! -d $TASK_NAME ]; then
+  mkdir $TASK_NAME
+  echo "makedir $GLUE_DIR/$TASK_NAME"
+fi
+cd $TASK_NAME
+if [ ! -f "d-train.json" ] || [ ! -f "m-train.json" ] || [ ! -f "d-dev.json" ] || [ ! -f "test1.1.json" ]; then
+  rm *
+  wget https://storage.googleapis.com/cluebenchmark/tasks/c3_public.zip
+  unzip c3_public.zip
+  rm c3_public.zip
+else
+  echo "data exists"
+fi
+echo "Finish download dataset."
+
+
+# make output dir
+if [ ! -d $CURRENT_DIR/${TASK_NAME}_output ]; then
+  mkdir -p $CURRENT_DIR/${TASK_NAME}_output
+  echo "makedir $CURRENT_DIR/${TASK_NAME}_output"
+fi
+
+# run task
+cd $CURRENT_DIR
 python run_c3.py \
   --gpu_ids="0,1,2,3" \
   --num_train_epochs=8 \
--- baselines/models_pytorch/mrc_pytorch/run_mrc_chid.sh
+++ baselines/models_pytorch/mrc_pytorch/run_mrc_chid.sh
@@ -7,6 +7,36 @@ export BERT_DIR=$OUTPUT_DIR/prev_trained_model/$MODEL_NAME
 export GLUE_DIR=$CURRENT_DIR/mrc_data # set your data dir
 TASK_NAME="CHID"
 
+# download and unzip dataset
+if [ ! -d $GLUE_DIR ]; then
+  mkdir -p $GLUE_DIR
+  echo "makedir $GLUE_DIR"
+fi
+cd $GLUE_DIR
+if [ ! -d $TASK_NAME ]; then
+  mkdir $TASK_NAME
+  echo "makedir $GLUE_DIR/$TASK_NAME"
+fi
+cd $TASK_NAME
+if [ ! -f "train.json" ] || [ ! -f "train_answer.json" ] || [ ! -f "dev.json" ] || [ ! -f "test1.1.json" ]; then
+  rm *
+  wget https://storage.googleapis.com/cluebenchmark/tasks/chid_public.zip
+  unzip chid_public.zip
+  rm chid_public.zip
+else
+  echo "data exists"
+fi
+echo "Finish download dataset."
+
+
+# make output dir
+if [ ! -d $CURRENT_DIR/${TASK_NAME}_output ]; then
+  mkdir -p $CURRENT_DIR/${TASK_NAME}_output
+  echo "makedir $CURRENT_DIR/${TASK_NAME}_output"
+fi
+
+# run task
+cd $CURRENT_DIR
 python run_multichoice_mrc.py \
   --gpu_ids="0,1,2,3" \
   --num_train_epochs=3 \
